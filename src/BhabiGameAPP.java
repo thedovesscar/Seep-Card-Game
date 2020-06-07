2761559d73552ee7d4b4f0289348e9bfebd3fbbb
Gurmehar <thedovesscar@gmail.com>
2020-06-06 18:45:47 -0700

    Currently including Deck instantiation in Main Method.
    needs to be relocated in the right location later.

--- src/BhabiGameAPP.java
+++ src/BhabiGameAPP.java
@@ -4,6 +4,9 @@ public class BhabiGameAPP {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new GameFrame();
+		
+		
+		// TODO needs to be transferred into its appropriate place
 		Deck.getInstance();
 	}
 
